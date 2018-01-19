ea57d36c6094e0c8730989bfaa5ca528d93a2329
butlerx <butlerx@notthe.cloud>
2018-01-19 15:20:02 +0000

    remove favour of toTop for sidebar

--- src/js/srolling.js
+++ src/js/srolling.js
@@ -1,10 +1,10 @@
-function smoothScroll(element, duration) {
+export function toElement(element, duration) {
   const getElementY = query =>
     window.pageYOffset + document.querySelector(query).getBoundingClientRect().top;
-  smoothScrollTo(getElementY(element), duration);
+  toY(getElementY(element), duration);
 }
 
-function smoothScrollTo(y, duration) {
+export function toY(y, duration) {
   const startingY = window.pageYOffset;
   // If element is close to page's bottom then window will scroll only to some position above the element.
   const diff =
@@ -27,64 +27,3 @@ function smoothScrollTo(y, duration) {
     }
   });
 }
-
-function fadeOut(arg) {
-  const el = document.getElementById(arg);
-  el.style.opacity = 1;
-
-  (function fade() {
-    const val = parseFloat(el.style.opacity) - 0.1;
-    el.style.opacity = val;
-    if (val < 0) {
-      el.style.display = 'none';
-    } else {
-      requestAnimationFrame(fade);
-    }
-  })();
-}
-
-function fadeIn(arg, display) {
-  const el = document.getElementById(arg);
-  el.style.opacity = 0;
-  el.style.display = display || 'inline-block';
-
-  (function fade() {
-    const val = parseFloat(el.style.opacity) + 0.1;
-    if (!(val > 1)) {
-      el.style.opacity = val;
-      requestAnimationFrame(fade);
-    }
-  })();
-}
-
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    const links = [...document.getElementsByClassName('sub-nav__link')];
-    links.forEach(link => {
-      link.addEventListener('click', smoothScroll.bind(null, link.hash, 1000));
-    });
-    const toTop =
-      document.getElementsByClassName('sidebar')[0].style.height - window.screen.height + 60;
-    window.addEventListener('scroll', () => {
-      if (document.width >= 800) {
-        const scrollTop =
-          window.pageYOffset !== undefined
-            ? window.pageYOffset
-            : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        if (scrollTop > toTop && scrollTop > 0) {
-          fadeIn('toTop');
-        } else {
-          fadeOut('toTop');
-        }
-      } else {
-        fadeIn('toTop');
-      }
-      fadeIn('toTop');
-    });
-    document.getElementById('toTop').addEventListener('click', () => {
-      smoothScrollTo(0, 600);
-    });
-  },
-  false,
-);
