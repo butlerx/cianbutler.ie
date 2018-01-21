a78b545bcb94fadea8d01e0717a17997e689e0a6
butlerx <butlerx@notthe.cloud>
2018-01-21 18:22:03 +0000

    handle unfefined

--- src/js/main.js
+++ src/js/main.js
@@ -9,15 +9,17 @@ document.addEventListener(
     [...document.getElementsByClassName('animation-target')][0].classList.remove(
       'animation-target',
     );
-    active[0].addEventListener('click', () => {
-      smoothScroll.toY(0, 1000);
-      if (!active[0].classList.contains('animation-target')) {
-        active[0].classList.add('animation-target');
-        setTimeout(() => {
-          active[0].classList.remove('animation-target');
-        }, 2000);
-      }
-    });
+    if (active[0]) {
+      active[0].addEventListener('click', () => {
+        smoothScroll.toY(0, 1000);
+        if (!active[0].classList.contains('animation-target')) {
+          active[0].classList.add('animation-target');
+          setTimeout(() => {
+            active[0].classList.remove('animation-target');
+          }, 2000);
+        }
+      });
+    }
     links.forEach(link => {
       link.addEventListener('click', smoothScroll.toElement.bind(null, link.hash, 1000));
     });
