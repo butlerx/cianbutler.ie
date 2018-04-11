a5628f6cc24ca90ca7dab769e1d5b6a962310beb
butlerx <butlerx@notthe.cloud>
2018-04-11 11:49:25 +0100

    fix scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,10 @@ document.addEventListener(
       'animation-target',
     );
     if (active[0]) {
-      active[0].addEventListener('click', () => {
-        smoothScroll.toY(0, 1000);
-        if (!active[0].classList.contains('animation-target')) {
-          active[0].classList.add('animation-target');
-          setTimeout(() => {
-            active[0].classList.remove('animation-target');
-          }, 2000);
-        }
-      });
+      active[0].addEventListener('click', () => smoothScroll.toY(0, 1000));
     }
     links.forEach(link => {
-      link.addEventListener('click', smoothScroll.toElement.bind(null, link.hash, 1000));
+      link.addEventListener('click', () => smoothScroll.toElement(link.hash, 1000));
     });
   },
   false,
