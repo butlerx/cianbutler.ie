dd23b4bbc5fa038a00c5138ef7347e8fb5f2cce0
butlerx <butlerx@notthe.cloud>
2019-06-02 23:20:48 +0100

    clean up scss

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -17,29 +17,32 @@ interface ExperienceProps {
   data: ExperienceData[];
 }
 
-export const Experience: SFC<ExperienceProps> = ({ data }) =>
-  data
-    .sort(
-      (a: ExperienceData, b: ExperienceData) =>
-        new Date(b.start).getTime() - new Date(a.start).getTime(),
-    )
-    .map(({ title, where, start, finish, languages, description }) => (
-      <div key={`${title}@${where}`}>
-        <h3 id={encodeURI(title)}>
-          {title}@{where}&emsp;
-          <small>
-            <em>
-              {formatDate(start)} - {formatDate(finish)}
-            </em>
-          </small>
-        </h3>
-        <Languages languages={languages} />
-        <blockquote>
-          <ReactMarkdown source={description} />
-        </blockquote>
-        <hr />
-      </div>
-    ));
+export const Experience: SFC<ExperienceProps> = ({ data }) => (
+  <div className='experience'>
+    {data
+      .sort(
+        (a: ExperienceData, b: ExperienceData) =>
+          new Date(b.start).getTime() - new Date(a.start).getTime(),
+      )
+      .map(({ title, where, start, finish, languages, description }) => (
+        <div key={`${title}@${where}`} className='experienceBlock'>
+          <h3 id={encodeURI(title)}>
+            {title}@{where}&emsp;
+            <small>
+              <em>
+                {formatDate(start)} - {formatDate(finish)}
+              </em>
+            </small>
+          </h3>
+          <Languages languages={languages} />
+          <blockquote>
+            <ReactMarkdown source={description} />
+          </blockquote>
+          <hr />
+        </div>
+      ))}
+  </div>
+);
 
 Experience.defaultProps = {
   data: [],
